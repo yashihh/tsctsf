94deb7ccc67bda8f007bc14d81fd47dc6e2d38ce
haoting <maggie.gcs09g@nctu.edu.tw>
2020-11-23 12:34:22 +0000

    fix(test): fix 5G GUTI octet 4 (four 1bits) in testfile - registration_test.go

--- test/registration_test.go
+++ test/registration_test.go
@@ -852,7 +852,7 @@ func TestGUTIRegistration(t *testing.T) {
 	// send NAS Deregistration Request (UE Originating)
 	GUTI5GS := nasType.MobileIdentity5GS{
 		Len:    11, // 5g-guti
-		Buffer: []uint8{0x02, 0x02, 0xf8, 0x39, 0xca, 0xfe, 0x00, 0x00, 0x00, 0x00, 0x01},
+		Buffer: []uint8{0xf2, 0x02, 0xf8, 0x39, 0xca, 0xfe, 0x00, 0x00, 0x00, 0x00, 0x01},
 	}
 	pdu = nasTestpacket.GetDeregistrationRequest(nasMessage.AccessType3GPP, 0, 0x04, GUTI5GS)
 	pdu, err = test.EncodeNasPduWithSecurity(ue, pdu, nas.SecurityHeaderTypeIntegrityProtectedAndCiphered, true, false)
