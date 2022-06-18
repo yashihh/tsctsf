cfd05aa20fed83b586f2771aba6f78b10e6e53d0
ianchen0119 <ychen.desl@gmail.com>
2022-06-18 14:21:34 +0000

    modify the go-upf bin name in shell script

--- test_multiUPF.sh
+++ test_multiUPF.sh
@@ -99,7 +99,7 @@ for i in $(seq -f "%02g" 1 $UPF_NUM); do
         TCPDUMP_PID_[${i}]=$(sudo ip netns pids "${UPFNS}${i}")
     fi
 
-    sudo -E ip netns exec "${UPFNS}${i}" ./bin/go-upf -c "${CONF_DIR}/multiUPF/upfcfg${i}.yaml" &
+    sudo -E ip netns exec "${UPFNS}${i}" ./bin/upf -c "${CONF_DIR}/multiUPF/upfcfg${i}.yaml" &
     sleep 1
 done
 
@@ -126,7 +126,7 @@ for ((idx=${#PID_LIST[@]}-1;idx>=0;idx--)); do
 done
 
 sleep 3
-sudo killall -15 go-upf
+sudo killall -15 upf
 sleep 1
 
 cd ..
