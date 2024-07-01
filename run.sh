5034789833c5b2008f6a3405ce66d4bc1d8689b1
yashihh <yashihh.11@nycu.edu.tw>
2024-07-01 16:52:09 +0800

    add tsctsf init

--- run.sh
+++ run.sh
@@ -109,10 +109,10 @@ UPF_PID=$(pgrep -P $SUDO_UPF_PID)
 PID_LIST+=($SUDO_UPF_PID $UPF_PID)
 
 mongo --eval "db.NfProfile.drop();db.applicationData.influenceData.subsToNotify.drop();db.applicationData.subsToNotify.drop();db.policyData.subsToNotify.drop();db.exposureData.subsToNotify.drop()" free5gc
-mongosh --eval "db.NfProfile.drop();db.applicationData.influenceData.subsToNotify.drop();db.applicationData.subsToNotify.drop();db.policyData.subsToNotify.drop();db.exposureData.subsToNotify.drop()" free5gc
+# mongosh --eval "db.NfProfile.drop();db.applicationData.influenceData.subsToNotify.drop();db.applicationData.subsToNotify.drop();db.policyData.subsToNotify.drop();db.exposureData.subsToNotify.drop()" free5gc
 sleep 0.1
 
-NF_LIST="nrf amf smf udr pcf udm nssf ausf nef"
+NF_LIST="nrf amf smf udr pcf udm nssf ausf nef tsctsf"
 
 export GIN_MODE=release
 
